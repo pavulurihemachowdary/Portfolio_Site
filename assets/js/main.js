9b3aa8494c07d0fdb3dfb71c4221aff959c651a5
pavulurihemachowdary <20bq1a05i4@vvit.net>
2024-10-06 10:43:23 +0530

    js_file

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,12 +97,12 @@ window.addEventListener('scroll',scrollActive)
 const sr=ScrollReveal({
     origin:'top',
     distance:'60px',
-    duration:2500,
-    delay:400,
+    duration:1500,
+    delay:200,
 })
 sr.reveal(`.home__data,.experience,.skills,.contact__container`)
-sr.reveal(`.home__img`,{delay:400})
-sr.reveal(`.home__scroll`,{delay:600})
-sr.reveal(`.work__card`,{interval:100})
+sr.reveal(`.home__img`,{delay:200})
+sr.reveal(`.home__scroll`,{delay:300})
+sr.reveal(`.work__card`,{interval:10})
 sr.reveal(`.about__content`,{origin:'right'})
 sr.reveal(`.about__img`,{origin:'left'})
